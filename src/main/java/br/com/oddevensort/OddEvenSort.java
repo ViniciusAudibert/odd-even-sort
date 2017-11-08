ee9516da4088685239ce72985f1d0bc320d554fb
Vinicius Audibert <vini.audibert@gmail.com>
2017-11-08 00:52:10 -0200

    testes

--- src/main/java/br/com/oddevensort/OddEvenSort.java
+++ src/main/java/br/com/oddevensort/OddEvenSort.java
@@ -1,55 +1,56 @@
 package br.com.oddevensort;
 
 public final class OddEvenSort {
-    private static Integer swapCounter;
-    
-    public static Integer getSwapCounter() {
-    	return swapCounter;
-    }
-    
-    /**
-     * @param array
-     */
-    public static <T extends Comparable<T>> void sort(T[] array) {
-        swapCounter = 0;
-        if (array != null) {
-            boolean sorted = false;
-            while (!sorted) {
-                sorted = innerSort(array, 1);
-                sorted = innerSort(array, 0) && sorted;
-            }
-        }
-    }
+	private static Long swapCounter;
 
-    /**
-     * @param array
-     * @param i
-     * @return
-     */
-    private static <T extends Comparable<T>> boolean innerSort(T[] array, Integer i) {
-        boolean sorted = true;
+	public static Long getSwapCounter() {
+		return swapCounter;
+	}
 
-        for (; i < array.length - 1; i += 2) {
-            if (array[i].compareTo(array[i + 1]) > 0) {
-                swap(array, i, i + 1);
-                sorted = false;
-            }
-        }
+	/**
+	 * Ordena o array passado por parametro
+	 * @param array
+	 */
+	public static <T extends Comparable<T>> void sort(T[] array) {
+		swapCounter = Long.valueOf(0);
+		if (array != null) {
+			boolean sorted = false;
+			while (!sorted) {
+				sorted = innerSort(array, 1);
+				sorted = innerSort(array, 0) && sorted;
+			}
+		}
+	}
 
-        return sorted;
-    }
+	/**
+	 * @param array
+	 * @param i
+	 * @return
+	 */
+	private static <T extends Comparable<T>> boolean innerSort(T[] array, Integer i) {
+		boolean sorted = true;
 
-    /**
-     * Método responsável por realizar a troca entre os arrays
-     *
-     * @param array
-     * @param a
-     * @param b
-     */
-    private static <T extends Comparable<T>> void swap(T[] array, int a, int b) {
-        T temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
-        swapCounter++;
-    }
+		for (; i < array.length - 1; i += 2) {
+			if (array[i].compareTo(array[i + 1]) > 0) {
+				swap(array, i, i + 1);
+				sorted = false;
+			}
+		}
+
+		return sorted;
+	}
+
+	/**
+	 * Método responsável por realizar a troca entre os arrays
+	 *
+	 * @param array
+	 * @param a
+	 * @param b
+	 */
+	private static <T extends Comparable<T>> void swap(T[] array, int a, int b) {
+		T temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
+		swapCounter++;
+	}
 }
--- src/test/java/br/com/oddevensort/OddEvenSortTest.java
+++ src/test/java/br/com/oddevensort/OddEvenSortTest.java
@@ -1,127 +1,144 @@
 package br.com.oddevensort;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.concurrent.ThreadLocalRandom;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OddEvenSortTest {
-	
-    @Test
-    public void sortArrayNull() {
-        // ARRANGE
-    	Integer[] nullArray = null;
-    	
-        // ACT
-    	OddEvenSort.sort(nullArray);
 
-        // ASSERT
-    	assertTrue(nullArray == null);
-    }
+	private final int arraySize = 10000;
 
-    @Test
-    public void sortEmptyArray() {
-        // ARRANGE
-    	Integer[] emptyArray = new Integer[0];
-    	
-        // ACT
-    	OddEvenSort.sort(emptyArray);
-    	
-        // ASSERT
-    	assertTrue(OddEvenSort.getSwapCounter() == 0);
-        assertTrue(emptyArray.length == 0);
-    }
-    
-    @Test
-    public void sortSortedArray() {
-        // ARRANGE
-    	Integer[] sortedArray = this.generateSortedArray(100);
-    	long startTime = System.nanoTime();
-    	
-        // ACT
-    	OddEvenSort.sort(sortedArray);
-    	long endTime = System.nanoTime();
-    	printInfo("Sorted Array (Best case)", endTime-startTime);
-    	
-        // ASSERT
-    	assertTrue(OddEvenSort.getSwapCounter() == 0);
-    }
-    
-    @Test
-    public void sortReverseSortedArray() {
-        // ARRANGE
-    	Integer[] reverseSortedArray = this.generateReverseSortedArray(100);
-    	long startTime = System.nanoTime();
-    	
-        // ACT
-    	OddEvenSort.sort(reverseSortedArray);
-    	long endTime = System.nanoTime();
-    	printInfo("Reverse Sorted Array (Worst case)", endTime-startTime);
-    	
-        // ASSERT
-    	assertTrue(OddEvenSort.getSwapCounter() == 4950);
-    }
-    
-    @Test
-    public void sortRandomArray() {
-        // ARRANGE
-    	Integer[] ramdomArray = this.generateRandomArray(100);
-    	long startTime = System.nanoTime();
-    	
-        // ACT
-    	OddEvenSort.sort(ramdomArray);
-    	long endTime = System.nanoTime();
-    	printInfo("Random Array", endTime-startTime);
-    	
-        // ASSERT
-    }
-    
-    // Aux functions
-    public void printInfo(String title, long totalTime) {
-    	System.out.println(title);
-    	System.out.print("Swaps = ");
-    	System.out.println(OddEvenSort.getSwapCounter());
-    	System.out.print("Total time = ");
-    	NumberFormat formatter = new DecimalFormat("#0.00000");
-    	System.out.println(formatter.format(totalTime / 1000000000d) + " seconds");
+	@Test
+	public void sortArrayNull() {
+		// ARRANGE
+		Integer[] nullArray = null;
 
-    }
-    
-    public Integer[] generateSortedArray(int size) {
-    	Integer[] array = new Integer[size];
-    	
-    	for(int i=0; i < size; i++) {
-    		array[i] = i;
-    	}
-    	
-    	return array;
-    }
-    
-    public Integer[] generateReverseSortedArray(int size) {
-    	Integer[] array = new Integer[size];
-    	int value = size;
-    	
-    	for(int i=0; i < size; i++) {
-    		array[i] = value;
-    		value--;
-    	}
-    	
-    	return array;
-    }
-    
-    public Integer[] generateRandomArray(int size) {
-    	Integer[] array = new Integer[size];
-    	
-    	for(int i=0; i < size; i++) {
-    		array[i] = ThreadLocalRandom.current().nextInt(0, size + 1);
-    	}
-    	
-    	return array;
-    }
+		// ACT
+		OddEvenSort.sort(nullArray);
+
+		// ASSERT
+		assertNull(nullArray);
+	}
+
+	@Test
+	public void sortEmptyArray() {
+		// ARRANGE
+		Integer[] emptyArray = new Integer[0];
+
+		// ACT
+		OddEvenSort.sort(emptyArray);
+
+		// ASSERT
+		assertEquals(OddEvenSort.getSwapCounter(), Long.valueOf(0));
+		assertEquals(emptyArray.length, 0);
+	}
+
+	@Test
+	public void sortSortedArray() {
+		// ARRANGE
+		Integer[] sortedArray = this.generateSortedArray(arraySize);
+		long startTime = System.nanoTime();
+
+		// ACT
+		OddEvenSort.sort(sortedArray);
+		long endTime = System.nanoTime();
+		printInfo("Sorted Array (Best case)", endTime - startTime);
+
+		// ASSERT
+		assertEquals(OddEvenSort.getSwapCounter(), Long.valueOf(0));
+		assertTrue(isArraySorted(sortedArray));
+	}
+
+	@Test
+	public void sortReverseSortedArray() {
+		// ARRANGE
+		Integer[] reverseSortedArray = this.generateReverseSortedArray(arraySize);
+		long startTime = System.nanoTime();
+
+		// ACT
+		OddEvenSort.sort(reverseSortedArray);
+		long endTime = System.nanoTime();
+		printInfo("Reverse Sorted Array (Worst case)", endTime - startTime);
+
+		// ASSERT
+		assertEquals(OddEvenSort.getSwapCounter(), Long.valueOf(49995000L));
+		assertTrue(isArraySorted(reverseSortedArray));
+	}
+
+	@Test
+	public void sortRandomArray() {
+		// ARRANGE
+		Integer[] ramdomArray = this.generateRandomArray(arraySize);
+		long startTime = System.nanoTime();
+
+		// ACT
+		OddEvenSort.sort(ramdomArray);
+		long endTime = System.nanoTime();
+		printInfo("Random Array", endTime - startTime);
+
+		// ASSERT
+		assertTrue(isArraySorted(ramdomArray));
+	}
+
+	// Aux functions
+	private void printInfo(String title, long totalTime) {
+		System.out.println(title);
+		System.out.print("Swaps = ");
+		System.out.println(OddEvenSort.getSwapCounter());
+		System.out.print("Total time = ");
+		NumberFormat formatter = new DecimalFormat("#0.00000");
+		System.out.println(formatter.format(totalTime / 1000000000d) + " seconds");
+		System.out.println();
+
+	}
+
+	private boolean isArraySorted(Integer[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private Integer[] generateSortedArray(int size) {
+		Integer[] array = new Integer[size];
+
+		for (int i = 0; i < size; i++) {
+			array[i] = i;
+		}
+
+		return array;
+	}
+
+	private Integer[] generateReverseSortedArray(int size) {
+		Integer[] array = new Integer[size];
+		int value = size;
+
+		for (int i = 0; i < size; i++) {
+			array[i] = value;
+			value--;
+		}
+
+		return array;
+	}
+
+	private Integer[] generateRandomArray(int size) {
+		Integer[] array = new Integer[size];
+
+		for (int i = 0; i < size; i++) {
+			array[i] = ThreadLocalRandom.current().nextInt(0, size + 1);
+		}
+
+		return array;
+	}
 }
